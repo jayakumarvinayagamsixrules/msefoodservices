9ced69ff1a64a57059a69d37076edda7a537bdd8
Jay <Jay@DESKTOP-UD19V1P>
2023-03-26 00:08:02 +0530

    Basket controller updated

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,19 @@
+using Basket.API.Repositories.Interfaces;
+using Basket.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Using the GetValue<type>(string key) method
+var redisServer = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+// or using the index property (which always returns a string)
+//var configValue = builder.Configuration["Authentication:CookieAuthentication:LoginPath"];
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = redisServer;
+});
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
