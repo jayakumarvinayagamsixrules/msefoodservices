80d90481b3999d2389ba655d550b0f47da0b143f
Jay <Jay@DESKTOP-UD19V1P>
2023-03-28 00:47:37 +0530

    Ordering api and CQRS projects added

---
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ordering.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        //public async Task<ActionResult<>> GetOrdersByUserName(string userName)
+        //{
+
+        //}
+    }
+}
